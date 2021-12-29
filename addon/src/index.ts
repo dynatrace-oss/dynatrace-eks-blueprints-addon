eb0ec8695bc4bf4df9c9e2521183008cce670d22
Thomas Schuetz <thomas.schuetz@dynatrace.com>
2021-12-29 09:24:26 +0100

    feat: added ssm integration
    Signed-off-by: Thomas Schuetz <thomas.schuetz@dynatrace.com>

--- addon/src/index.ts
+++ addon/src/index.ts
@@ -1,33 +1,48 @@
 import { ClusterAddOn, ClusterInfo } from '@aws-quickstart/ssp-amazon-eks';
 import { Construct } from '@aws-cdk/core'
-import {loadExternalYaml, readYamlDocument} from "@aws-quickstart/ssp-amazon-eks/dist/utils";
-import * as cluster from "cluster";
+import {getSecretValue, loadExternalYaml, readYamlDocument} from "@aws-quickstart/ssp-amazon-eks/dist/utils";
 
 /**
  * Configuration options for the add-on.
  */
 
+interface DtSecret {
+    API_URL: string;
+    API_TOKEN: string;
+    PAAS_TOKEN: string;
+}
+
+
 export class DynatraceOperatorAddOn implements ClusterAddOn {
     readonly Namespace: string;
     readonly HelmRepository: string;
-    readonly ApiToken: string;
-    readonly ApiUrl: string;
-    readonly PaasToken: string;
+    private ApiToken: string;
+    private ApiUrl: string;
+    private PaasToken: string;
     readonly CustomResourceUrl: string;
+    readonly SSMSecretName: string;
 
-    constructor(apiToken?: string, apiUrl?: string, paasToken?: string, namespace?: string, helmrepo?: string, customResourceUrl?: string) {
+    constructor(ssmSecretName?: string, apiToken?: string, apiUrl?: string, paasToken?: string, namespace?: string, helmrepo?: string, customResourceUrl?: string) {
+        this.SSMSecretName = ssmSecretName ?? ""
         this.ApiToken = apiToken ?? ""
         this.ApiUrl = apiUrl ?? ""
         this.PaasToken = paasToken ?? ""
         this.Namespace = namespace ?? "dynatrace";
         this.HelmRepository = helmrepo ?? "https://raw.githubusercontent.com/Dynatrace/helm-charts/master/repos/stable"
-        this.CustomResourceUrl = "https://github.com/Dynatrace/dynatrace-operator/releases/latest/download/dynatrace.com_dynakubes.yaml"
-
+        this.CustomResourceUrl = customResourceUrl ?? "https://github.com/Dynatrace/dynatrace-operator/releases/latest/download/dynatrace.com_dynakubes.yaml"
     }
 
     async deploy(clusterInfo: ClusterInfo): Promise<Construct> {
         const crdManifest: Record<string,any>[] = loadExternalYaml(this.CustomResourceUrl);
         const manifest = clusterInfo.cluster.addManifest("DynaKubeCustomResource", ...crdManifest)
+
+        if (this.SSMSecretName != "") {
+            const secretValue = await getSecretValue(this.SSMSecretName!, clusterInfo.cluster.stack.region);
+            let credentials: DtSecret = JSON.parse(secretValue)
+            this.ApiUrl = credentials.API_URL
+            this.ApiToken = credentials.API_TOKEN
+            this.PaasToken = credentials.PAAS_TOKEN
+        }
 
         const operatorHelmChart = clusterInfo.cluster.addHelmChart("dynatrace-operator", {
             chart: "dynatrace-operator",
@@ -44,6 +59,6 @@ export class DynatraceOperatorAddOn implements ClusterAddOn {
 
         operatorHelmChart.node.addDependency(manifest)
         return operatorHelmChart
-
     }
+
 }
